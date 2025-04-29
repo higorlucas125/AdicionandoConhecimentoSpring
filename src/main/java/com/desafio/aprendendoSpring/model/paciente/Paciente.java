60787473045f5c685cf05abf2f993ab0f5df18a0
higorlucas125 <higorlaf@hotmail.com>
2025-04-28 22:51:09 -0300

    atualização no Paciente.java que estava com erro

--- src/main/java/com/desafio/aprendendoSpring/model/paciente/Paciente.java
+++ src/main/java/com/desafio/aprendendoSpring/model/paciente/Paciente.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Table(name = "pacientes")
 @Entity(name = "Paciente")
